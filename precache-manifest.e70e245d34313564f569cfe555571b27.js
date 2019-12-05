0d714a0b311bb0fab9a659197fe9bccd3b8808df
oscar flores <oscar.flores@globant.com>
2019-12-05 17:08:16 -0600

    Updates

---
+++ precache-manifest.e70e245d34313564f569cfe555571b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a87d1e4225a3bb760f51139e2073289",
+    "url": "/demo_reactjs/index.html"
+  },
+  {
+    "revision": "c4167831966e445ef6a0",
+    "url": "/demo_reactjs/static/css/main.92f72a80.chunk.css"
+  },
+  {
+    "revision": "ab3d3c25f3d519a331cc",
+    "url": "/demo_reactjs/static/js/2.da8ebd2e.chunk.js"
+  },
+  {
+    "revision": "c4167831966e445ef6a0",
+    "url": "/demo_reactjs/static/js/main.c9ad0009.chunk.js"
+  },
+  {
+    "revision": "d7d8d3b1207900a7bf1c",
+    "url": "/demo_reactjs/static/js/runtime-main.2cdcde34.js"
+  }
+]);
